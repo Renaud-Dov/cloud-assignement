d608c75dee8962030a70cebb4f79dc1cb1b1ec38
Dov DEVERS <dov@chavers.org>
2022-03-31 16:59:39 +0100

    removed header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {Home} from "./components/Home";
 import {About} from "./components/About";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            {/* <Header/> */}
             <Routes>
                 <Route path="/" element={<Home/>}>
                     <Route path="about" element={<About/>}/>
