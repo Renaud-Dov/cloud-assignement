037c04e6c839822836dc57591f67a1b69a238469
Dov DEVERS <dov@chavers.org>
2022-04-06 14:38:20 +0100

    updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,7 @@ const other_links = [
 const all_links = [...links, ...other_links]
 
 export default function Header() {
-    return (<Popover className="relative bg-white">
+    return (<Popover as="header" className="relative bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div
                 className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
@@ -54,7 +54,7 @@ export default function Header() {
                     <Popover.Button
                         className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
                         <span className="sr-only">Open menu</span>
-                        <MenuIcon className="h-6 w-6" aria-hidden="true"/>
+                        <MenuIcon className="h-8 w-8" aria-hidden="true"/>
                     </Popover.Button>
                 </div>
                 <Popover.Group as="nav" className="hidden md:flex space-x-10">
@@ -135,9 +135,11 @@ export default function Header() {
                     className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                     <div className="pt-5 pb-6 px-5">
                         <div className="flex items-center justify-between">
-                            <div>
-                                <MusicNoteIcon className="h-12 w-12 text-orange-500"/>
-                            </div>
+                            <Popover.Button>
+                                <Link to={"/"}>
+                                    <MusicNoteIcon className="h-12 w-12 text-orange-500"/>
+                                </Link>
+                            </Popover.Button>
                             <div className="-mr-2">
                                 <Popover.Button
                                     className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
@@ -146,41 +148,30 @@ export default function Header() {
                                 </Popover.Button>
                             </div>
                         </div>
-                        <div className="mt-6">
-                            <nav className="grid gap-y-8">
-                                {/*{solutions.map((item) => (*/}
-                                {/*    <a*/}
-                                {/*        key={item.name}*/}
-                                {/*        href={item.href}*/}
-                                {/*        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"*/}
-                                {/*    >*/}
-                                {/*        <item.icon className="flex-shrink-0 h-6 w-6 text-orange-600"*/}
-                                {/*                   aria-hidden="true"/>*/}
-                                {/*        <span*/}
-                                {/*            className="ml-3 text-base font-medium text-gray-900">{item.name}</span>*/}
-                                {/*    </a>*/}
-                                {/*))}*/}
-                            </nav>
-                        </div>
                     </div>
                     <div className="py-6 px-5 space-y-6">
                         <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                            {all_links.map((item, index) => (<Link key={index} to={item.link}
-                                                                   className="text-base font-medium text-gray-900 hover:text-gray-700">
-                                {item.name}
-                            </Link>))}
+                            {all_links.map((item, index) => (
+                                <Popover.Button key={index}
+                                                className="text-base text-left font-medium text-gray-900 hover:text-gray-700">
+                                    <Link to={item.link}>
+                                        {item.name}
+                                    </Link>
+                                </Popover.Button>))}
                         </div>
                         {/*login/signup mobile*/}
                         <div>
-                            <Link to={"/signup"}
-                                  className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-orange-600 hover:bg-orange-700">
-                                Sign up
-                            </Link>
+                            <Popover.Button
+                                className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-orange-600 hover:bg-orange-700">
+                                <Link to={"/signup"}> Sign up </Link>
+                            </Popover.Button>
                             <p className="mt-6 text-center text-base font-medium text-gray-500">
                                 Existing customer?{' '}
-                                <Link to={"/login"} className="text-orange-600 hover:text-orange-500">
-                                    Sign in
-                                </Link>
+                                <Popover.Button>
+                                    <Link to={"/login"} className="text-orange-600 hover:text-orange-500">
+                                        Sign in
+                                    </Link>
+                                </Popover.Button>
                             </p>
                         </div>
                     </div>
