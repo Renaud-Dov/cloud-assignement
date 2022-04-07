489509f323a0c712694dd0801548ea4d4c53146e
Dov DEVERS <dov@chavers.org>
2022-04-07 13:41:58 +0100

    updated Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -36,24 +36,26 @@ function addToBasket(id_product) {
 
 function Card(element) {
     return <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <Link to={`product/${element.id_product}`}>
+        <Link to={`/product/${element.id_product}`}>
             <img className="p-8 rounded-t-lg" src={element.img} alt={element.alt}/>
         </Link>
         <div className="px-5 pb-5">
-            <Link to={`product/${element.id_product}`}>
+            <Link to={`/product/${element.id_product}`}>
                 <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{element.name}</h5>
             </Link>
             <Rating className="flex items-center mt-2.5 mb-5" value={element.rating}/>
 
             <div className="flex justify-between items-center">
                 <span className="text-3xl font-bold text-gray-900 dark:text-white">{element.price}</span>
-                <div onClick={() => {
+                <button onClick={() => {
                     addToBasket(element.id_product)
                 }}
-                   className="text-white flex bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                    to basket
+                        className="text-white flex bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none
+                              focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center
+                              dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Add to basket
                     <PlusIcon className="ml-2 block h-5 w-5"/>
-                </div>
+                </button>
 
 
             </div>
