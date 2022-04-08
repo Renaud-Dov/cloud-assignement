58cd6a9abcfdb90ba5fc03e6b9c61c385bfd5b70
Dov DEVERS <dov@chavers.org>
2022-04-08 02:51:51 +0100

    added loading products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,35 +1,35 @@
 import {Link} from "react-router-dom";
-import {PlusIcon} from '@heroicons/react/solid'
+import {PlusIcon, StarIcon} from '@heroicons/react/solid'
 import {Rating} from "./Rating";
 import {useEffect, useState} from "react";
 import {ReadAll} from "../database";
+import axios from "axios";
 
-const list = [
-    {
-        name: 'Product 1',
-        id_product: 1,
-        price: '$100',
-        img: 'https://flowbite.com/docs/images/products/product-1.png',
-        alt: 'Product 1 image',
-        rating: 3,
-    },
-    {
-        name: 'Product 2',
-        id_product: 2,
-        price: '$200',
-        img: 'https://flowbite.com/docs/images/products/product-1.png',
-        alt: 'Product 2 image',
-        rating: 4,
-    },
-    {
-        name: 'Product 3',
-        id_product: 3,
-        price: '$300',
-        img: 'https://flowbite.com/docs/images/products/product-1.png',
-        alt: 'Product 3 image',
-        rating: 2,
-    }
-];
+function LoadingCard() {
+    const stars = []
+    for (let i = 0; i < 3; i++) stars.push(<StarIcon key={i} className="w-5 h-5 text-slate-400"/>)
+    for (let i = 3; i < 5; i++) stars.push(<StarIcon key={i} className="w-5 h-5 text-slate-200"/>)
+    return (<div className="bg-slate-50  rounded-lg shadow-md">
+            <div className="animate-pulse">
+                <div className="p-8 rounded-t-lg bg-slate-400 h-52 w-full"/>
+                <div className="px-5 py-5 space-y-3 p-3">
+                    {/*title*/}
+                    <div className="h-3 w-20 bg-slate-400 rounded"/>
+                    {/*rating*/}
+                    <div className="flex items-center">
+                        {stars}
+                    </div>
+                    <div className="flex justify-between items-center">
+                        {/*price*/}
+                        <div className="h-3 w-20 bg-slate-400 rounded"/>
+                        {/*add to cart*/}
+                        <div className="rounded-lg bg-slate-400 h-10 w-40 px-5 py-2.5"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 function addToBasket(id_product) {
 }
@@ -37,7 +37,8 @@ function addToBasket(id_product) {
 function Card(element) {
     return <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <Link to={`/product/${element.id}`}>
-            <img className="p-8 rounded-t-lg" src={'https://flowbite.com/docs/images/products/product-1.png'} alt={"DickJohnson"}/>
+            <img className="p-8 rounded-t-lg" src={'https://flowbite.com/docs/images/products/product-1.png'}
+                 alt={"DickJohnson"}/>
         </Link>
         <div className="px-5 pb-5">
             <Link to={`/product/${element.id}`}>
@@ -65,14 +66,37 @@ function Card(element) {
 
 export default function Products() {
     const [elements, setElements] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
     useEffect(() => {
-        ReadAll().then(
-            (l) => setElements(l)
-        ).catch( (error) =>
-            console.log("couldn't fetch data", error)
-        )
+        setLoading(true);
+        axios.get('https://api.cloud.itsligo.bugbear.fr/api/v1/items')
+            .then(res => {
+                setLoading(false);
+                setElements(res.data)
+
+            })
+            .catch(err => {
+                setLoading(false);
+                setError(true);
+                console.log(err)
+            })
     }, []);
-    return <div className="flex mt-4 flex-wrap gap-4 justify-center">
-        {elements.map(element => <Card key={element.id} {...element}/>)}
-    </div>
+    return <>
+        <div className="">
+            {loading ?
+                <div className="fixed flex mt-4 flex-wrap gap-4 justify-center">
+                    {[...Array(30)].map((_, i) => <LoadingCard key={i}/>)}
+                </div> : error ?
+                    <div className="w-full h-full flex justify-center items-center">
+                        <div className="text-red-700">
+                            Error
+                        </div>
+                    </div> :
+                    <div className="flex mt-4 flex-wrap gap-4 justify-center">
+                        {elements.map(element => <Card key={element.id} {...element}/>)}
+                    </div>}
+        </div>
+
+    </>
 }
--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 import {CosmosClient} from "@azure/cosmos";
 import {v4 as uuidv4} from "uuid"
-
+import axios from "axios"
 const partitionKey = {kind: 'Hash', paths: ['/partitionKey']}
 
 const options = {
     endpoint: "https://axov.documents.azure.com:443/",
-    key: process.env.DB_KEY,
+    key: "ndNjiWc4xuvGj7XXytxRRy98p29WrK4H3ioNuM11W4hK4nAM0pgxuH7snKfmc6xkuzildePEiVipzWTxbIDrpQ==",
     userAgentSuffix: 'CosmosDBJavascriptQuickstart',
     connectionPolicy: {
         enableEndpointDiscovery: false
@@ -18,8 +18,15 @@ const client = new CosmosClient(options)
 
 //ReadAll
 export async function ReadAll() {
-    const items = await client.database("ToDoList").container("Items").items.readAll().fetchAll()
-    return items.resources
+    axios.get('https://api.cloud.itsligo.bugbear.fr/api/v1/items')
+        .then(res => {
+            return res.data
+        })
+        .catch(err => {
+            console.log(err)
+
+        })
+
 }
 
 //ReadItem
